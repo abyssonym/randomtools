2bfa253d550028d72efe14827639dade5e261524
Anonymous <none>
2021-07-10 07:51:18 -0700

    read_lines_nocomment only strips trailing whitespace; map_to_lorom uses high bank addressing

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def read_lines_nocomment(filename):
         for line in f:
             if '#' in line:
                 line, _ = line.split('#', 1)
-            line = line.strip()
+            line = line.rstrip()
             if not line:
                 continue
             lines.append(line)
@@ -497,16 +497,20 @@ def rewrite_snes_checksum(filename, lorom=False):
 
 
 def map_to_lorom(address):
+    if address > 0x3fffff:
+        raise Exception('LOROM address out of range.')
     base = ((address << 1) & 0xFFFF0000)
     lorom_address = base | 0x8000 | (address & 0x7FFF)
-    return lorom_address
+    assert (map_from_lorom(lorom_address & 0x7fffff) ==
+            map_from_lorom(lorom_address | 0x800000))
+    return lorom_address | 0x800000
 
 
 def map_from_lorom(lorom_address):
     lorom_address &= 0x7FFFFF
     base = (lorom_address >> 1) & 0xFFFF8000
     address = base | (lorom_address & 0x7FFF)
-    assert lorom_address == map_to_lorom(address)
+    #assert lorom_address == map_to_lorom(address)
     return address
 
 
